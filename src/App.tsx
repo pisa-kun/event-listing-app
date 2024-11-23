e2c4b3261763e71dc1e4bd1bb8f905236ed3feac
pisa-kun <pisa0401mst@gmail.com>
2024-11-23 17:19:59 +0900

    feat: eventDetail

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import EventList from "./pages/EventList";
+import EventDetail from "./pages/EventDetail";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Navigate to="/events" />} />
+        {/* 子要素になるので暗黙的にevents/:id となる */}
           <Route path="events" element={<Layout />}>
           <Route index element={<EventList />} />
           {/* 動的な値に応じたURL */}
-          <Route path="events/:id" element={<p>イベント詳細を表示</p>} />
-          <Route path="events/:id/apply" element={<p>イベントに申し込む</p>} />
-          <Route path="events/:id/confirm" element={<p>申し込み完了です</p>} />
+          <Route path=":id" element={<EventDetail/>} />
+          <Route path=":id/apply" element={<p>イベントに申し込む</p>} />
+          <Route path=":id/confirm" element={<p>申し込み完了です</p>} />
         </Route>
         {/* 存在しないURLの処理 */}
         <Route path="*" element={<p>404 not found</p>} />
---
+++ src/pages/EventDetail.tsx
@@ -0,0 +1,73 @@
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { EVENTS as events } from "../constants";
+import REACT_EVENT from "../assets/react-event.jpg";
+import { CalendarDays, MapPin, Users } from "lucide-react";
+import Card from "../components/Card";
+
+const EventDetail = () => {
+  const { id } = useParams();
+  const event = events.find((e) => e.id === id);
+
+  if (!event) {
+    return (
+      <>
+        <p>URLのイベントが見つかりません</p>
+        <Link to="/" className="underline">
+          イベント一覧に戻る
+        </Link>
+      </>
+    );
+  }
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex gap-8 max-w-5xl mx-auto">
+      <Card>
+        <h1 className="text-4xl font-bold mb-4 text-gray-800">{event.title}</h1>
+        <p className="whitespace-pre-wrap">{event.description}</p>
+        <img
+          src={REACT_EVENT}
+          alt={event.title}
+          className="rounded-t-lg w-[660px] h-[440px]"
+        />
+      </Card>
+      <div className="">
+        <Card>
+          <h2 className="text-2xl font-bold mb-4 text-gray-800">
+            イベントの詳細
+          </h2>
+          <div className="flex flex-wrap gap-4 text-base">
+            <div className="flex items-center">
+              <CalendarDays className="w-4 h-4 mr-2 text-blue-500" />
+              <span>{event.date}〜</span>
+            </div>
+            <div className="flex items-center">
+              <MapPin className="w-4 h-4 mr-2 text-green-500" />
+              <span>{event.location}</span>
+            </div>
+            <div className="flex items-center">
+              <Users className="w-4 h-4 mr-2 text-purple-500" />
+              <span>
+                {event.attendees}
+                {event.capacity && `/${event.capacity}`}
+                人参加予定
+              </span>
+            </div>
+          </div>
+
+          <Link to={`/events/${event.id}/apply`} className="block">
+            <button
+              // onClick={() => navigate(`/events/${event.id}/apply`)}
+              className="py-4 w-full bg-sky-600 hover:opacity-80 text-white rounded-xl"
+            >
+              申し込む
+            </button>
+          </Link>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default EventDetail;
